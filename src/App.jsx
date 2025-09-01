f7bd4dda12375db7931b6ae13456df9a831ee480
Deepak Negi <negideepak0479@example.com>
2025-09-01 14:06:40 +0530

    update App.jsx with public and private route access

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,51 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { RecoilRoot } from 'recoil';
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import Dashboard from "./pages/Dashboard";
 import HeroSection from "./pages/HeroSection";
+import { AuthProvider, useAuth } from "./context/AuthContext";
+
 function App() {
   return (
-    <RecoilRoot>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HeroSection />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </BrowserRouter>
-    </RecoilRoot>
+    <AuthProvider>
+      <RecoilRoot>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HeroSection />} />
+            <Route path="/login" element={<PublicRoute>
+              <LoginPage />
+            </PublicRoute>} />
+            <Route path="/signup" element={
+              <PublicRoute> <SignupPage /></PublicRoute>
+            } />
+            <Route path="/dashboard" element={<ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>} />
+            <Route path="*" element={<Navigate to="/" />} />
+
+          </Routes>
+        </BrowserRouter>
+      </RecoilRoot>
+    </AuthProvider>
   );
+}
+
+function PublicRoute({ children }) {
+  const { isAuth } = useAuth();
+
+  if (isAuth === null) {
+    return <div className="flex h-screen items-center justify-center">Loading...</div>;
+  }
+
+  return isAuth ? <Navigate to="/dashboard" /> : children;
+}
+
+function ProtectedRoute({ children }) {
+  const { isAuth } = useAuth();
+
+  if (isAuth === null) return <div>Loading..</div>;
+  return isAuth ? children : <Navigate to={"/login"} />;
 }
 
 export default App;
